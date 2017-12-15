c623b494c0eceee04fe787e4fffe493fc899dba5
EarnCoin <lagendre@gmail.com>
2017-12-15 15:45:14 +0800

    update scroll top

--- js/sb-admin-2.js
+++ js/sb-admin-2.js
@@ -54,5 +54,23 @@ $(function() {
 		console.error('Trigger:', e.trigger);
 	});
 
+	
+	
 });
 
+//scroll top
+window.onscroll = function() {scrollFunction()};
+
+	function scrollFunction() {
+		if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+			document.getElementById("myBtn").style.display = "block";
+		} else {
+			document.getElementById("myBtn").style.display = "none";
+		}
+	}
+
+	// When the user clicks on the button, scroll to the top of the document
+	function topFunction() {
+		document.body.scrollTop = 0;
+		document.documentElement.scrollTop = 0;
+	}
